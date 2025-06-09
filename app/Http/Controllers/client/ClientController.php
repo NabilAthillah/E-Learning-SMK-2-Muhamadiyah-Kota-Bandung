d4680f1b797fb0866eb795e171f9d08d80f5ff67
lulzxz <zakyarifrahman83@gmail.com>
2025-06-10 02:49:30 +0700

    origin/Branch-Zak
    Mata Pelajaran, Footer

--- app/Http/Controllers/client/ClientController.php
+++ app/Http/Controllers/client/ClientController.php
@@ -3,20 +3,36 @@
 namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kelas;
+use App\Models\MataPelajaran;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Contracts\Role;
 
 class ClientController extends Controller
 {
-    public function index() {
-        return view('client.index');
+    public function index()
+    {
+        $mataPelajaran = MataPelajaran::all();
+        $kelas = Kelas::all();
+        return view('client.index', compact('mataPelajaran', 'kelas'));
     }
 
-    public function getGuru() {
-        return view('client.guru.index'); }
+    public function getGuru()
+    {
+        $guru = User::role('guru')->get();
+        return view('client.guru.index', compact('guru'));
 
-    public function getSatu() {
-        return view('client.aktifitas.index'); }
-    public function getKursus() {
-        return view('client.kursus.index');
+    }
+
+    public function getSatu()
+    {
+        return view('client.aktifitas.index');
+    }
+    public function getKursus()
+    {
+        $mataPelajaran = MataPelajaran::all();
+        $kelas = Kelas::all();
+        return view('client.kursus.index', compact('mataPelajaran', 'kelas'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,5 @@ Route::name('client.')->controller(ClientController::class)->group(function () {
     Route::get('/', 'index')->name('home');
     Route::get('/guru', 'getGuru')->name('guru');
     Route::get('/bihalal', 'getSatu')->name('bihalal');
-});
-
-
     Route::get('/kursus', 'getKursus')->name('kursus');
 });
